b8be175c2c511691eaa4ff58d15f6e88704a87b4
yuyatinnefeld <yuyawamo@gmail.com>
2023-06-06 07:43:24 +0200

    faet: update prometheus go app scraping

--- instrumentator/golang/main.go
+++ instrumentator/golang/main.go
@@ -4,22 +4,64 @@ import (
     "fmt"
     "log"
     "net/http"
+    "github.com/prometheus/client_golang/prometheus"
     "github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	requestCount *prometheus.CounterVec
+	cpuUsage     *prometheus.GaugeVec
 )
 
 func homePage(w http.ResponseWriter, r *http.Request){
     fmt.Fprintf(w, "Welcome to the HomePage!")
-    fmt.Println("Endpoint Hit: homePage")
+
+    if r.URL.Path == "/" {
+		requestCount.WithLabelValues("home_page").Inc()
+
+        cpuUsage.WithLabelValues("home_page").Set(0.75)
+	}
+}
+
+func metricsPage(w http.ResponseWriter, r *http.Request){
+    if r.URL.Path == "/metrics" {
+		requestCount.WithLabelValues("metrics").Inc()
+	}
+
+    promhttp.Handler().ServeHTTP(w, r)
+
 }
 
 func handleRequests() {
-    http.HandleFunc("/", homePage)
-	http.Handle("/metrics", promhttp.Handler())
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	requestCount = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+		    Name: "go_app_requests_count",
+            Help: "Total App HTTP Requests count",
+		},
+		[]string{"endpoint"},
+	)
 
+	cpuUsage = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "go_app_cpu_usage",
+			Help: "CPU usage",
+		},
+		[]string{"endpoint"},
+	)
+
+	prometheus.MustRegister(requestCount)
+	prometheus.MustRegister(cpuUsage)
+
+	server := http.NewServeMux()    
+	server.HandleFunc("/", homePage)
+	server.HandleFunc("/metrics", metricsPage)
+
+	port := ":8080"
+	fmt.Println("Server started on port", port)
+    log.Fatal(http.ListenAndServe(port, server))
 }
 
 func main() {
-    fmt.Println("Rest API v2.0 - Golang")
+    fmt.Println("Golang Rest API Starting...")
     handleRequests()
 }
